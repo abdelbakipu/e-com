c76e2ee5e04980415200806a409db4e46da13295
Abdelbaki Boucebha <abdelbakipu@gmail.com>
2026-07-04 15:34:10 +0100

    added accounts serializers, views, urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .serializer import UserProfileSerializer, UserRegisterSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # Create your views here.
+
+User = get_user_model()
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
