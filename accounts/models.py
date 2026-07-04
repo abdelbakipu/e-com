947607fde0c3742eb12bdb423b9d893924940c14
Abdelbaki Boucebha <abdelbakipu@gmail.com>
2026-07-04 15:10:49 +0100

    added custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
