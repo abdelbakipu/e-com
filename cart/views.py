24458d33f57e4f450675013d8d2a15a343a589cf
Abdelbaki Boucebha <abdelbakipu@gmail.com>
2026-07-14 15:39:47 +0100

    added cart views

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,26 @@
-from rest_framework.viewsets import generics,
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .serializers import CartItemSerializer, CartSerializer
+from .models import Cart, CartItem
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
+class CartView(generics.RetrieveAPIView):
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        cart, created = Cart.objects.get_or_create(user = self.request.user)
+        return cart
+
+class CartItemView(ModelViewSet):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user = self.request.user)
+    
+    def perform_create(self, serializer):
+        cart, created = Cart.objects.get_or_create(user = self.request.user)
+        serializer.save(cart = cart)
